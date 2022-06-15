4dedf73f147e928744c73ab837df2a0f50b63eb3
pritish <daspritish99@gmail.com>
2022-06-15 22:26:20 +0530

    api completed

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,11 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 # Create your models here.
 
 class USerProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
     def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+
         if not email:
             raise ValueError("User must have a email address")
 
@@ -19,6 +24,8 @@ class USerProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
@@ -29,6 +36,8 @@ class USerProfileManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+
     email = models.EmailField(max_length=256, unique=True)
     name = models.CharField(max_length=256)
     is_active = models.BooleanField(default=True)
@@ -40,10 +49,27 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
+        """Retrieve full name of user"""
         return self.name
 
     def get_short_name(self):
+        """Retrieve shot name of user"""
         return self.name
 
     def __str__(self):
+        """Return string representation of our user"""
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,17 +1,41 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import render
+
 from . import serializers
+from . import models
+from . import permissions
 # Create your views here.
 
+def index(request):
+    my_dic = {'insert_me':"Hello Welcome To The API World!"}
+    return render(request,'myapp/index.html',context=my_dic)
+
+
 class HelloApiView(APIView):
+    """Test API View"""
     serializer_class = serializers.HelloSerializer
 
-
     def get(self, request, format=None):
-        return Response({"Message":'Hello!'})
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({"message":'Hello!'})
 
     def post(self, request):
+        """Create a hello message with our name"""
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
@@ -25,10 +49,84 @@ class HelloApiView(APIView):
             )
 
     def put(self, request, pk=None):
+        """Handle updating an object"""
         return Response({'method': 'PUT'})
 
     def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
         return Response({'method': 'PATCH'})
 
     def delete(self,request, pk=None):
+        """Delete an object"""
         return Response({'method': 'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [
+            'Uses actions (list, create,retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message':'Hello!'})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+            )
+
+    def retrive(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
